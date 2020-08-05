ec285a06a482e5eff45a5e7f9d6521673e69b286
Jordan Muncrief <jordean89@gmail.com>
2020-08-05 15:53:27 -0600

    Declared options object. Added options alerts and if statements.

--- script.js
+++ script.js
@@ -10,5 +10,41 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+  var options = {};
+
+  options.length = parseInt(prompt("How long would you like your password to be?"));
+  if (options.length < 8 || options.length > 128) {
+    alert("Alert goes here.");
+    return;
+  }
+  options.special = confirm("Would you like to include special characters?");
+  options.numeric = confirm("Would you like to include numeric characters?");
+  options.uppercase = confirm("Would you like to include uppercase characters?");
+  options.lowercase = confirm("Would you like to include lowercase characters?");
+
+  for (var i = 0; i < options.length; i++) {
+
+    if (options.special) {
+
+    }
+
+    if (options.numeric) {
+
+    }
+
+    if (options.uppercase) {
+
+    }
+
+    if (options.lowercase) {
+
+    }
+
+  }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
